e5516e29943599bdbdb5dbbb57788f4aa8418a11
Mattèo Gauthier <matteo.gauthier@gmail.com>
2019-05-01 18:08:36 +0200

    Référencement, tooltips, TODOs, cleaning, SEO ↗

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # TODO
 - [ ] Changer la rotation du bg des boxItem
 - [ ] Media queries for monitor with w > 3784px
+- [ ] Host Custom Font
---
+++ logic.js
@@ -0,0 +1,20 @@
+tippy('.tick-icon', {
+    content: 'Projet terminé, toujours maintenu',
+    theme: 'light',
+    placement: 'bottom'
+  })
+tippy('.work-icon', {
+    content: 'Projet en construction',
+    theme: 'light',
+    placement: 'bottom'
+  })
+tippy('.github-icon', {
+    content: 'Github Repository',
+    theme: 'light',
+    placement: 'bottom'
+  })
+tippy('.icon-twitter', {
+    content: 'Mon Twitter',
+    theme: 'light',
+    placement: 'bottom'
+  })
